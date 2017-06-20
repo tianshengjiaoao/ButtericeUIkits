14bacef50687452a4274adc8d7670c321d75710e
wangweijun <wangshi199030@gmail.com>
2017-06-20 20:01:09 +0800

    first commit

---
+++ js/example.js
@@ -0,0 +1,33 @@
+function clickRadio1(){
+  document.getElementById('r1').click()
+  };
+function clickRadio2(){
+  document.getElementById('r2').click()
+  };
+function clickRadio3(){
+  document.getElementById('r3').click()
+  };
+function clickRadio4(){
+  document.getElementById('r4').click()
+  };
+function clickRadio5(){
+  document.getElementById('r5').click()
+  };
+function checkbox1(){
+  document.getElementById('c1').click()
+  };
+function checkbox2(){
+  document.getElementById('c2').click()
+  };
+function checkbox3(){
+  document.getElementById('c3').click()
+  };
+document.getElementById('bu1').onclick=clickRadio1;
+document.getElementById('bu2').onclick=clickRadio2;
+document.getElementById('bu3').onclick=clickRadio3;
+document.getElementById('bu4').onclick=clickRadio4;
+document.getElementById('bu5').onclick=clickRadio5;
+
+document.getElementById('bc1').onclick=checkbox1;
+document.getElementById('bc2').onclick=checkbox2;
+document.getElementById('bc3').onclick=checkbox3;
